3b1f46a7d96c734e2b9590b787b6ab449667341f
Vivi357 <1552729475@qq.com>
2021-03-17 02:17:33 +0800

    Update test_BRF.R

--- lib/test_BRF.R
+++ lib/test_BRF.R
@@ -2,11 +2,9 @@
 ### Make predictions with test features ###
 ###########################################################
 
-test <- function(model, features){
+test_BRF <- function(model, features){
   res <- predict(model, 
                  data.frame(features))
   return(res)
 }
 
-# This function is not necessary.
-# We put it here just to show the structure.
